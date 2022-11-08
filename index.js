cd61cdea5f39241f190068cf470aa3a44996e0a7
Maxim Postnikov <max@postnikov.dev>
2022-11-08 22:14:18 +0300

    final base version

---
+++ index.js
@@ -0,0 +1,4 @@
+import { createThemeSwitcher } from './theme-switcher.js';
+
+
+createThemeSwitcher(75, 45, 13, 2000, './day.jpg', './night.jpg', 'night')
--- theme-switcher.js
+++ theme-switcher.js
@@ -1,9 +1,9 @@
 const _themeSwitcher = document.querySelector(".theme-switcher");
-const _day = _themeSwitcher.querySelector(".day");
-const _night = _themeSwitcher.querySelector(".night");
-const _contentSwitcher = _themeSwitcher.querySelector(".content-switcher");
+let _contentSwitcher;
+let theme;
 
-let theme = _contentSwitcher.classList.contains('theme_day') ? 'day' : 'night'
+//let theme = _contentSwitcher.classList.contains('theme_day') ? 'day' : 'night'
+//let theme = 'night'
 //const themeSwitchSpeed = 2000;
 
 
@@ -19,38 +19,165 @@ const switcher = (classRemove, classAdd, delay) => {
 }
 
   
-const changeTheme = (themeSwitchSpeed) => {
-    if (theme === "night") {
+const changeTheme = (duration, currentTheme) => {
+    if (currentTheme === "night") {
         switcher('', 'theme_day_1', 0)
-        .then(() => switcher('theme_day_1', 'theme_day_2', themeSwitchSpeed / 4))
+        .then(() => switcher('theme_day_1', 'theme_day_2', duration / 4))
         .then(() => switcher('theme_day_2', 'theme_day', 30))
         theme = 'day'
     } else {
-        switcher('theme_day', 'theme_day_back-1', 0)
-        .then(() => switcher('theme_day_back-1', 'theme_day_back-2', themeSwitchSpeed / 4))
-        .then(() => switcher('theme_day_back-2', '', 30))
+        switcher('theme_day', 'theme_day_back_1', 0)
+        .then(() => switcher('theme_day_back_1', 'theme_day_back_2', duration / 4))
+        .then(() => switcher('theme_day_back_2', '', 30))
         theme = 'night'
     }
 }
 
-const createThemeSwitcher = (width, height, circleSize, duration) => {
+
+
+
+
+const createThemeSwitcherStyles = (width, height, circleSize, duration, pathDayImg, pathNightImg) => {
+    let circlePosition = width / 2 - circleSize;
 
     const styleEl = document.createElement('style');
     document.head.appendChild(styleEl);
     const styleThemeSwitcher = styleEl.sheet;
 
     styleThemeSwitcher.insertRule(`
-    .content-switcher{
-        width: ${width}px;
-        height: ${height}px;
-        border-radius: ${height / 2}px;
-        position: relative;
-        overflow: hidden;
-    }
-    `)
+        .theme-switcher > .content-switcher {
+            width: ${width}px;
+            height: ${height}px;
+            border-radius: ${height / 2}px;
+            position: relative;
+            overflow: hidden;
+        }`)
+
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher > div {
+            position: absolute;
+            height: 100%;
+            width: 100%;
+        }`)
+
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher > div.day {
+            background-image: url('${pathDayImg}');
+            background-size: contain;
+            clip-path: circle(${circleSize}px at ${circlePosition}px 50%);
+            transition: ${duration/4}ms cubic-bezier(0,1,0,1);
+        }`)
+
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher > div.night {
+            background-image: url('${pathNightImg}');
+            background-size: contain;
+            transition: ${duration/4}ms cubic-bezier(0,1,0,1);
+        }`)
+
+
+
+    //theme day_1
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_1 .day {
+            transition: ${duration/4}ms cubic-bezier(1,0,1,0);
+            clip-path: circle(${width*10}px at ${circlePosition - width * 10 + circleSize}px 50%);
+        }`)
+       
+
+
+
+    //theme day_2
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_2 .day {
+            transition: ${duration/4}ms cubic-bezier(1,0,1,0);
+            clip-path: circle(${width*10}px at ${circlePosition - width * 10 + circleSize}px 50%);
+        }`)
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_2 .night {
+            transition: ${duration/4}ms cubic-bezier(1,0,1,0);
+            clip-path: circle(${width*10}px at ${circlePosition + width * 10 + circleSize}px 50%);
+        }`)
+
+
+    
+    //theme day
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day .day {
+            transition: ${duration/4}ms cubic-bezier(0,1,0,1);
+            z-index: 900;
+            clip-path: circle(${width*10}px at ${circleSize - width * 9}px 50%);
+        }`)
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day .night {
+            transition: ${duration/4}ms cubic-bezier(0,1,0,1);
+            z-index: 1000;
+            clip-path: circle(${circleSize}px at ${circlePosition + circleSize * 2}px 50%);
+        }`)
+
+
+
+
+    //theme day_back_1
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_back_1 .day {
+            transition: 0ms;
+            z-index: 900;
+            clip-path: circle(${width*10}px at ${circleSize - width * 9}px 50%);
+
+        }`)
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_back_1 .night {
+            transition: ${duration/4}ms cubic-bezier(1,0,1,0);
+            z-index: 1000;
+            clip-path: circle(${width * 10}px at ${circlePosition + circleSize + width * 10}px 50%);
+        }`)
+
+
+
+    //theme day_back_2
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_back_2 .day {
+            transition: 0ms;
+            z-index: 1000;
+            clip-path: circle(${width*10}px at ${circlePosition - width * 10 + circleSize}px 50%);
+        }`)
+        
+    styleThemeSwitcher.insertRule(`
+        .theme-switcher > .content-switcher.theme_day_back_2 .night {
+            transition: 0ms;
+            z-index: 900;
+            clip-path: circle(${width * 10}px at ${circlePosition + circleSize + width * 10}px 50%);
+        }`)
+}
+
+const createThemeSwitcherHtml = (currentTheme) => {
+    document.querySelector('.theme-switcher').innerHTML = `
+        <div class="content-switcher ${currentTheme !== 'night' ? 'theme_day' : null}">
+            <div class="night"></div>
+            <div class="day"></div>
+        </div>
+    `;
+    _contentSwitcher = _themeSwitcher.querySelector(".content-switcher");
 }
 
 
-createThemeSwitcher(100, 50, 18, 2000);
 
-_themeSwitcher.addEventListener('click', () => changeTheme(2000));
+
+
+//defaultTheme: 'night' -> night, other/undef -> day
+
+export const createThemeSwitcher = (width, height, circleSize, duration, pathDayImg, pathNightImg, defaultTheme) => {
+    theme = defaultTheme;
+    new Promise((res, rej) => {
+        createThemeSwitcherHtml(defaultTheme);
+        createThemeSwitcherStyles(width, height, circleSize, duration, pathDayImg, pathNightImg);
+        res()
+    })
+    .then(_themeSwitcher.addEventListener('click', () => changeTheme(duration, theme)))
+}
+
+
+
+
+
